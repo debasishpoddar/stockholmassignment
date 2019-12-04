ec0ff4e2a9c68ce87f5841bae34daf9c6e81a8e6
debasishpoddar <32933669+debasishpoddar@users.noreply.github.com>
2019-12-04 21:32:16 +0530

    Delete tempdataprocessrun.sh

--- stk-evndata-processor/tempdataprocessrun.sh
+++
@@ -1,8 +0,0 @@
-./bin/spark-submit --class com.stklm.core.processor.TemparatureDataProcessor \
-    --master yarn \
-    --num-executors 1 \
-    --driver-memory 1GB \
-    --executor-memory 2GB \
-    --executor-cores 2 \
-     {JAR_LOCATION}/stk-evndata-processor-0.0.1-SNAPSHOT.jar \
-    {HDFS_DATA_LOCATION}/stklm_temparature/
